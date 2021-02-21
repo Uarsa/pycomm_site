87950c9ead9023e021e9e39dca7bdd2e29b43bb7
Uarsa <rap41na300311>
2021-02-21 13:29:15 +0300

    Rename html templates. Add temporary variable filename.

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import datetime
+import os
 
 
 UPLOAD_FOLDER = "files/questionnaires/"
-ALLOWED_EXTENSIONS = {'txt', }
+ALLOWED_EXTENSIONS = {'txt', 'py', }
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+filename = ""
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -16,39 +18,56 @@ def index():
         name = request.form['name']
         email = request.form['email']
         date = datetime.datetime.now().strftime('%d.%m.%Y %R')
+        global filename
+        filename = email.split(".")[0]
         data = name + "\n" + email + "\n" + date
-        f = open(UPLOAD_FOLDER + email + ".txt", "w", encoding="utf-8")
+        f = open(UPLOAD_FOLDER + filename + "_bio.txt", "w", encoding="utf-8")
         f.write(data)
         f.close()
 
-        return redirect('form1.html')
+        return redirect('questionnaire.html')
     else:
         return render_template('index.html')
 
 
-@app.route('/form1.html', methods=['POST', 'GET'])
+@app.route('/questionnaire.html', methods=['POST', 'GET'])
 def form1():
     if request.method == "POST":
         knowledge = request.form['knowledge']
         yourself = request.form['yourself']
-        filename = "test"
-        f = open(UPLOAD_FOLDER + filename + ".txt", "w", encoding="utf-8")
-        f.write(knowledge + "\n \n" + yourself)
+        global filename
+        f = open(UPLOAD_FOLDER + filename + "_bio.txt", "a", encoding="utf-8")
+        f.write("\nИспользовать:\n" + knowledge + "\nО себе:\n" + yourself)
         f.close()
 
-        return redirect('form2.html')
+        return redirect('tasks.html')
     else:
-        return render_template('form1.html')
+        return render_template('questionnaire.html')
 
 
-@app.route('/form2.html', methods=['POST', 'GET'])
+@app.route('/tasks.html', methods=['POST', 'GET'])
 def form2():
     if request.method == "POST":
-        pass
+        global filename
+        filename = filename.split(".")[0] + "_"
+        task_1 = request.files["task1"]
+        if task_1:
+            task_1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename + task_1.filename))
+        task_2 = request.files["task2"]
+        if task_2:
+            task_2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename + task_2.filename))
+        task_3 = request.files["task3"]
+        if task_3:
+            task_3.save(os.path.join(app.config['UPLOAD_FOLDER'], filename + task_3.filename))
 
-        return redirect('form2.html')
+        return redirect('success.html')
     else:
-        return render_template('form2.html')
+        return render_template('tasks.html')
+
+
+@app.route('/success.html')
+def success():
+    return render_template('success.html')
 
 
 if __name__ == '__main__':
